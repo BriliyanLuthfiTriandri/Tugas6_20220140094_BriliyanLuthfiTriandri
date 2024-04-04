1646f30859e5e67b31cb6d6922d22b79e9395792
BriliyanluthfiTriandri <briliyanlt@gmail.com>
2024-04-04 16:45:05 +0700

    codingan js

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini Video Mamang Racing");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Ini Audio Mamang Racing");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("Ini Gambar Mamang Racing");
+});
